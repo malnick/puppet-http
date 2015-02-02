57471c4eb85fe075e3f39667e30bc9b3b8a26ccb
malnick <malnick@gmail..com>
2015-02-01 18:08:37 -0800

    done

--- manifests/listener.pp
+++ manifests/listener.pp
@@ -1,11 +1,3 @@
-#    webhook::listener { $route:
-#            port         => $listen_port,
-#            bind_address => $bind_address,
-#            rack_end     => $rack_env,
-#            http_method  => $http_method,
-#            command      => 'puppet agent -t',
-#    }
-
 define webhook::listener (
 
     $route              = $name, 
@@ -23,7 +15,7 @@ define webhook::listener (
     File {
         mode  => '0755',
         group => 'root',
-        user  => 'root',
+        owner => 'root',
     }
 
     file {"/usr/local/bin/webhook_${name}":
@@ -37,7 +29,7 @@ define webhook::listener (
     file {"webhook_${name}.rb":
         path    => "/usr/local/bin/webhook_${name}/lib/webhook_${name}.rb",
         ensure  => file,
-        content => template('webhook/webhook'),
+        content => template('webhook/simple_webhook.rb.erb'),
     }
 
     file {"/usr/local/bin/webhook_${name}/logs":
@@ -56,7 +48,7 @@ define webhook::listener (
     service {"webhook_${name}":
         ensure   => running,
         start    => "/opt/puppet/bin/ruby /usr/local/bin/webhook_${name}/bin/run",
-        stop     => "/bin/kill -9 ${/usr/bin/lsof -i :${port} | awk '{print $2}' | tail -1)",
+        stop     => "/bin/kill -9 $(/usr/bin/lsof -i :${port} | awk '{print \$2}' | tail -1)",
         provider => 'base',
         require  => File["/usr/local/bin/webhook_${name}/bin/run", "webhook_${name}.rb"],
     }

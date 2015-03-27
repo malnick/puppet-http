18b5342aa2011946f52f7696581a0a54daa7b9b3
malnick <malnick@gmail..com>
2015-03-27 11:05:41 -0700

    provider/http/http.rb

--- lib/puppet/provider/http/http.rb
+++ lib/puppet/provider/http/http.rb
@@ -15,13 +15,17 @@ desc "Run pupppet on a node"
   end
 
   def post
-    port    = resource[:port]
-    route   = resource[:route]
-    fqdn    = resource[:fqdn]
-    json    = resource[:data].to_json
-    url     = "http://#{fqdn}:#{port}/#{route}"
-    uri     = URI(url)
-    Net::HTTP.post_form(uri, json)
+    port      = resource[:port]
+    route     = resource[:route]
+    fqdn      = resource[:fqdn]
+    json      = resource[:data].to_json
+    url       = "http://#{fqdn}:#{port}/#{route}"
+    uri       = URI(url)
+    req       = Net::HTTP::Post.new(uri, initheader = {'Content-Type' =>'application/json'})
+    http      = Net::HTTP.new(uri.host, uri.port)
+    req.body  = json
+    http.request(req)
+    #Net::HTTP.post_form(uri, json)
   end
 
   def exists?
